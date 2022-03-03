bd1041fbd85192146de4c4ee97f41603a653f5dd
crhunt <cassihunt@gmail.com>
2022-03-02 20:03:47 -0600

    highlighting roleboxes

--- docs/lib/utilities.js
+++ docs/lib/utilities.js
@@ -1,6 +1,20 @@
 // Initialize all the utilities required by the web-app.
 
+var ormjs;
+
 function webapp_utilities() {
     highlight_listener();
     activate_vdragbar("vdragbar","container","left_box");
+}
+
+function set_traversal(view) {
+
+    console.log("set_traversal", view, view.id)
+
+    if(d3.select("#traversalMode").property("checked")){
+        view.traversal = true;
+    } else {
+        view.traversal = false;
+    }
+    ormjs.Traversal.update(view);
 }
