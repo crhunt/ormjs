7ad99e957afe93246ed5531a90e53b61f3a0acd5
Hunt <35147718+crhunt@users.noreply.github.com>
2022-08-11 19:47:55 -0500

    Font support (#51)
    * Add symbola font and use as backup for arrow rendering
    
    * minify css and js
    
    * quick start guide
    
    * finish quick start doc
    
    * small code cleanup
    
    * subtype preferred to quick start

---
+++ style/minify.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+uglifycss ormjs-variables.css ormjs-orm-style.css ormjs-popup-style.css ormjs-traversal.css --output ormjs.style.min.css
+
+uglifycss ormjs-variables.css ormjs-rai-style-1.css ormjs-orm-style.css ormjs-popup-style.css ormjs-traversal.css --output ormjs.style.rai1.min.css
+
+uglifycss ormjs-variables.css ormjs-rai-style-2.css ormjs-orm-style.css ormjs-popup-style.css ormjs-traversal.css --output ormjs.style.rai2.min.css
